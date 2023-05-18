4456083116bd03f227a891fee8136deb342e3511
Hederson Sousa <hedersonbalbino@gmail.com>
2023-05-17 21:52:14 -0300

    Aula React
    estudando mais sobre os fundamentos do react

---
+++ estudando-react/reactapp/src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+
+import './styles.css';
+
+
+export function App () {
+  return (
+    <div ClassName="container">
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder="Digite um nome.." />
+      <button type="button">Adicionar</button>
+    </div>
+
+  )
+}
+
